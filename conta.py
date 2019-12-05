e18ce52aefb94dfea2bbed426a4bfef14607866e
Vitor <vitor.condexd@gmail.com>
2019-12-05 10:38:44 -0300

    Adicionando arquivos

---
+++ conta.py
@@ -0,0 +1,20 @@
+
+class Conta:
+
+    #__init__ é o constructor
+    #self é a referencia do objeto na memória
+    def __init__(self, numero, titular, saldo, limite):
+        print("Construindo o Objeto... {}".format(self))
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
+
+    def extrato(self):
+        print("Titular : {} <-> Saldo : {}".format(self.__titular, self.__saldo))
+
+    def deposita(self, valor):
+        self.__saldo += valor
+
+    def saca(self, valor):
+        self.__saldo -= valor
